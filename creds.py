11585f9719a98160202510b865b9c479e5140d6e
moeroddy <mrmoe@bu.edu>
2018-04-08 17:23:01 -0400

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,4 +1,6 @@
-consumer_key = "50T0wOLSptEl3qTJXVG2oL9zD"
-consumer_secret = "AhDJtsDmqJLZACKaWAoqfNCiluzXot06JjFA0sGXURLMvTKbGF"
-access_token = "960659026173456384-NTHJ2NEvMKSsDZQ27cd2PBkQXchvHj5"
-access_token_secret = "Bz3V4El6xMldc7WiEP9fwzQg9hsDXjoapyYXqFYASgPoy"
+#consumer_key = "50T0wOLSptEl3qTJXVG2oL9zD"
+#consumer_secret = "AhDJtsDmqJLZACKaWAoqfNCiluzXot06JjFA0sGXURLMvTKbGF"
+#access_token = "960659026173456384-NTHJ2NEvMKSsDZQ27cd2PBkQXchvHj5"
+#access_token_secret = "Bz3V4El6xMldc7WiEP9fwzQg9hsDXjoapyYXqFYASgPoy"
+
+THIS IS JUST AN EXAMPLE, PLEASE USE YOUR OWN CREDS 
