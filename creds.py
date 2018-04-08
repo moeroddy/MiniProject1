3e75a0af84431efd070c5d05b28d7037e35d8b9d
moeroddy <mrmoe@bu.edu>
2018-04-08 17:22:26 -0400

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,4 @@
+consumer_key = "50T0wOLSptEl3qTJXVG2oL9zD"
+consumer_secret = "AhDJtsDmqJLZACKaWAoqfNCiluzXot06JjFA0sGXURLMvTKbGF"
+access_token = "960659026173456384-NTHJ2NEvMKSsDZQ27cd2PBkQXchvHj5"
+access_token_secret = "Bz3V4El6xMldc7WiEP9fwzQg9hsDXjoapyYXqFYASgPoy"
